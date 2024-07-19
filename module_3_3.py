3767fac1395bb5a1c5b78759a8862ec2526226be
JDH-993 <sonya.shlyakhova@mail.ru>
2024-07-19 09:51:34 +0700

    Update module_3_3.py

--- module_3_3.py
+++ module_3_3.py
@@ -14,8 +14,9 @@ values_dict[key1], values_dict[key2], values_dict[key3] = (
 
 values_list_2 = [float(input("введите число ")), input("введите строку ")]
 
-print_params(*values_list_2, 42)
+
 print_params(b = 25)
 print_params(c = [1, 2, 3])
 print_params(*values_list)
 print_params(**values_dict)
+print_params(*values_list_2, 42)
